c74381dfe77999fd1dca452168c8453c3fba3e7e
Duaa Zahi <duaa-zahi@hotmail.com>
2020-04-03 23:40:47 +0400

    remove import

--- lib/deadlineApp.dart
+++ lib/deadlineApp.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'homePage.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 class DeadlineApp extends StatelessWidget {
   // This widget is the root of your application.
